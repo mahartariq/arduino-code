5b6f3f28f79cc02ae1d8ba7a229c01939ba42041
mahartariq <mahartariq499@gmail.com>
2022-01-01 00:12:46 +0500

    MQ-7 code added .

---
+++ MQ-7 Sensor/sketch_jan01a.ino
@@ -0,0 +1,20 @@
+    /*
+  modified on Sep 28, 2020
+  Modified by MohammedDamirchi from Arduino Examples
+  Home
+*/
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // print out the value you read:
+  Serial.println(sensorValue);
+  delay(2000);
+}
